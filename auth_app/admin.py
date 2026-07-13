fbe259628d1429676127c3630890ae2939bac942
Felix R <nikojin@gmx.net>
2026-07-13 10:56:20 +0200

    add admin panel

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    """Admin configuration for CustomUser, adapted for email-based authentication."""
+    model = CustomUser
+    list_display = ("email", "username", "is_active", "is_staff", "date_joined")
+    list_filter = ("is_active", "is_staff")
+    search_fields = ("email", "username")
+    ordering = ("-date_joined",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_active", "is_staff"),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
