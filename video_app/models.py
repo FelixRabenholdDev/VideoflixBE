6401a581afb71c6d3acdea6b6d8292d3c24db32d
Felix R <nikojin@gmx.net>
2026-07-14 09:40:14 +0200

    upd video categories

--- video_app/models.py
+++ video_app/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class Video(models.Model):
+    class Category(models.TextChoices):
+        DRAMA = "Drama", "Drama"
+        ROMANCE = "Romance", "Romance"
+        COMEDY = "Comedy", "Comedy"
+        ACTION = "Action", "Action"
+        DOCUMENTARY = "Documentary", "Documentary"
+
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     thumbnail = models.ImageField(upload_to="thumbnails/", blank=True, null=True)
     video_file = models.FileField(upload_to="uploads/")
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=Category.choices)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
